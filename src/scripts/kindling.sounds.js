eac537c03e067e4fefc4e39498e751c79555e2d5
Scott Schmerer <thrombo19@hotmail.com>
2011-12-16 15:53:18 -0500

    Adding the ability to play custom sounds statically stored in a list in the customsounds.js

--- src/scripts/kindling.sounds.js
+++ src/scripts/kindling.sounds.js
@@ -1,32 +1,27 @@
-kindling.module(function () {
+(function () {
 	'use strict';
 
 	var SOUNDS = {
-		'Crickets chirping': 'crickets',
-		'Do it live!': 'live',
-		'Drama': 'drama',
-		'Great job': 'greatjob',
-		'Nyan cat': 'nyan',
-		'Push it': 'pushit',
-		'Rimshot': 'rimshot',
-		'Sad trombone': 'trombone',
-		'Secret area': 'secret',
-		'Ta-da!' : 'tada',
-		'The More You Know': 'tmyk',
-		'Yeeeaaah!': 'yeah',
-		'Vuvuzela': 'vuvuzela'
+		'Crickets chirping': '/play crickets',
+		'Cross fingers': '/customplay crossFingers',
+		'Do it live!': '/play live',
+		'Do it stage!': '/customplay stage',
+		'Drumroll': '/customplay drumroll',
+		'Drama': '/play drama',
+		'Go Cloud Team!': '/customplay goCloudTeam',
+		'Great job': '/play greatjob',
+		'Push it': '/play pushit',
+		'Rimshot': '/play rimshot',
+		'Sad trombone': '/play trombone',
+		'Secret area': '/play secret',
+		'Ta-da!' : '/play tada',
+		'The More You Know': '/play tmyk',
+		'Yeeeaah!': '/play yeah',
+		'Vuvuzela': '/play vuvuzela'
 	};
 
-	var MENU_ID = 'soundButton-wrapper';
-
-	function displayMenu() {
-		if (document.getElementById(MENU_ID)) {
-			return;
-		}
-		$('#chat_controls').append('<div id="' + MENU_ID + '" class="tooltip">\
-				<img id="soundButton" title="' + chrome.i18n.getMessage('soundMenuTooltip') + '" src="' + chrome.extension.getURL('img/sound.gif') + '" width="18" height="15" />\
-				<span id="soundContainer" class="tooltip-inner"></span>\
-		</div>');
+	$(function () {
+		$('#chat_controls').append('<div id="soundButton-wrapper" class="tooltip"><img id="soundButton" title="' + chrome.i18n.getMessage('soundMenuTooltip') + '" src="' + chrome.extension.getURL("img/sound.gif") + '" width="18" height="15" /><span id="soundContainer" class="tooltip-inner"></span></div>');
 
 		var $soundButton = $('#soundButton');
 		var $soundContainer = $('#soundContainer');
@@ -46,23 +41,9 @@ kindling.module(function () {
 		$soundContainer.children('.sound').click(function () {
 			var input = document.getElementById('input');
 			var oldValue = input.value;
-			input.value = '/play ' + this.getAttribute('data-value');
+			input.value = this.getAttribute('data-value');
 			document.getElementById('send').click();
 			input.value = oldValue;
 		});
-	}
-
-	var onOptionsChanged = function (e, options) {
-		if (options.soundAndEmojiMenus === 'true') {
-			displayMenu();
-		} else {
-			$('#' + MENU_ID).remove();
-		}
-	};
-
-	return {
-		init: function () {
-			$.subscribe('optionsChanged', onOptionsChanged);
-		}
-	};
+	});
 }());
