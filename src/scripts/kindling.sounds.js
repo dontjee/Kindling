1db6cb4b85f06bdee291d5eb7a74ac1e785bd80e
Scott Schmerer <thrombo19@hotmail.com>
2012-01-13 16:40:35 -0500

    Fixing a bad merge from the Jensen

--- src/scripts/kindling.sounds.js
+++ src/scripts/kindling.sounds.js
@@ -21,18 +21,18 @@ kindling.module(function () {
 		'Vuvuzela': '/play vuvuzela'
 	};
 	
-	var onOptionsChanged = function (e, options) {
-		if (options.soundAndEmojiMenus === 'true') {
-			displayMenu();
-		} else {
-			$('#' + MENU_ID).remove();
-		}
-	};
-		
-	return {
-		init: function () {
-			$('#chat_controls').append('<div id="soundButton-wrapper" class="tooltip"><img id="soundButton" title="' + chrome.i18n.getMessage('soundMenuTooltip') + '" src="' + chrome.extension.getURL("img/sound.gif") + '" width="18" height="15" /><span id="soundContainer" class="tooltip-inner"></span></div>');
-
+	var MENU_ID = 'soundButton-wrapper';
+	
+	var displayMenu = function() {
+			//$('#chat_controls').append('<div id="soundButton-wrapper" class="tooltip"><img id="soundButton" title="' + chrome.i18n.getMessage('soundMenuTooltip') + '" src="' + chrome.extension.getURL("img/sound.gif") + '" width="18" height="15" /><span id="soundContainer" class="tooltip-inner"></span></div>');
+			if (document.getElementById(MENU_ID)) {
+				return;
+			}
+			$('#chat_controls').append('<div id="' + MENU_ID + '" class="tooltip">\
+			<img id="soundButton" title="' + chrome.i18n.getMessage('soundMenuTooltip') + '" src="' + chrome.extension.getURL('img/sound.gif') + '" width="18" height="15" />\
+			<span id="soundContainer" class="tooltip-inner"></span>\
+			</div>');
+			
 			var $soundButton = $('#soundButton');
 			var $soundContainer = $('#soundContainer');
 			var sound;
@@ -55,6 +55,18 @@ kindling.module(function () {
 				document.getElementById('send').click();
 				input.value = oldValue;
 			});
+	};
+	
+	var onOptionsChanged = function (e, options) {
+		if (options.soundAndEmojiMenus === 'true') {
+			displayMenu();
+		} else {
+			$('#' + MENU_ID).remove();
+		}
+	};
+		
+	return {
+		init: function () {
 			$.subscribe('optionsChanged', onOptionsChanged);
 		}
 	};
